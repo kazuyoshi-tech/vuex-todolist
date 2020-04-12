c12d71a0b293f1665e6be874ace1e5f959fecec1
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-04-12 17:39:42 +0900

    コメント投稿完了

---
+++ src/components/Board.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>
+      Todo-List
+    </header>
+    <main>
+      <list-add />
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :listIndex="index"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+import List from './List'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    ListAdd,
+    List,
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+  },
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const savedLists = localStorage.getItem('todo-lists')
+
+const store = new Vuex.Store({
+  state: {
+      lists: savedLists ? JSON.parse(savedLists): [
+        {
+          title: 'Backlog',
+        },
+        {
+          title: 'Todo',
+        },
+        {
+          title: 'Doing',
+        }
+      ],
+  },
+  mutations: {
+    addlist(state, payload) {
+        state.lists.push({ title: payload.title, cards:[] })
+    },
+    removelist(state, payload) {
+      state.lists.splice(payload.listIndex, 1)
+    },
+  },
+  actions: {
+    addlist(context, payload) {
+        context.commit('addlist', payload)
+    },
+    removelist(context, payload) {
+      context.commit('removelist', payload)
+    },
+  },
+  getters: {
+  },
+})
+
+store.subscribe((mutation, state) => {
+    localStorage.setItem('todo-lists', JSON.stringify(state.lists))
+})
+
+export default store
